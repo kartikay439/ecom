30ac08a881ef4898333a9ceb99f490f49470b8e5
kartikay439 <kartikaytrivedi99@gmail.com>
2024-10-25 02:40:14 +0530

    cloudinary unlink problem solved

--- src/router/user.route.js
+++ src/router/user.route.js
@@ -7,7 +7,7 @@ import {verifyJWT} from '../middleware/auth.middleware.js'
 const router = Router()
 
 
-router.route("/register").post(
+router.route("/register").pos(
     upload.fields([
         {
             name:"avatar",
